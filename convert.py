1370cc18b87a5e150d689024cd6f387517500c01
mltn123 <62905344+mltn123@users.noreply.github.com>
2022-07-22 13:21:13 +0200

    readme

---
+++ convert.py
@@ -0,0 +1,126 @@
+import pandas as pd
+import numpy as np
+import json
+import ast
+import csv
+import os
+import sys
+from pathlib import Path
+from random import randint
+import random
+import requests  # pip3 install requests
+from pprint import pprint
+
+
+# GET = requests.get
+# POST = requests.post
+#
+# # if using Heroku, change this to https://YOURAPP.herokuapp.com
+# SERVER_URL = 'http://localhost:8000'
+# REST_KEY = ''  # fill this later
+#
+# def call_api(method, *path_parts, **params) -> dict:
+#     path_parts = '/'.join(path_parts)
+#     url = f'{SERVER_URL}/api/{path_parts}/'
+#     resp = method(url, json=params, headers={'otree-rest-key': REST_KEY})
+#     if not resp.ok:
+#         msg = (
+#             f'Request to "{url}" failed '
+#             f'with status code {resp.status_code}: {resp.text}'
+#         )
+#         raise Exception(msg)
+#
+#     return resp.json()
+#
+#
+# #data = call_api(GET, 'sessions', 'ez8qjxjg', participants_codes=['3s01ruvl'])
+# data = call_api(GET, 'participant_vars', 'ez8qjxjg', vars=dict(Code='3s01ruvl'))
+# pprint(data)
+
+def random_with_N_digits(p,n):
+    random.seed(p) # defines same digit for same participant on every run
+    range_start = 10**(n-1)
+    range_end = (10**n)-1
+    return randint(range_start, range_end)
+
+
+filename = sys.argv[1]
+
+mydata = pd.read_csv(filename)
+temp = mydata.loc[:, mydata.columns.str.startswith('embodyOtree..*.player.pyField')]
+
+participantCode =  mydata['participant.code'].values
+
+
+subjects_path = Path.cwd() / 'subjects'
+subjects_path.mkdir(parents=True, exist_ok=True)
+
+for p in participantCode:
+    id = random_with_N_digits(p,6) #random digits because thats what matlab wants
+    temp = mydata.loc[mydata['participant.code'] == p]
+    # Huge mess but it works
+    presentation = temp.loc[:, temp.columns.str.endswith('player.rand_page_sequence')].copy()
+    presentation.dropna(how='all', axis=1, inplace=True)
+    presentation = presentation.values
+    presentation = np.array2string(presentation, precision=2, separator=',',suppress_small=False)
+    presentation = presentation.replace("[","")
+    presentation = presentation.replace("]","")
+    presentation = presentation.replace("'", "")
+    presentation = presentation.replace(" ", "")
+    presentation = presentation.split(",")
+    presentation = list(map(int, presentation))
+    presentation =  [x-1 for x in presentation] #matlab expects values from 0 to 13 , not 1 to 14
+    presentation = ','.join(str(x) for x in presentation)
+    presentation = presentation.replace(",","\n")
+
+
+
+    temp = temp.loc[:, temp.columns.str.contains('player.pyField') ]
+    temp.dropna(how='all', axis=1, inplace=True)
+    temp.columns = temp.columns.str.rstrip('\.\d*?\.')
+
+    participant_path = Path.cwd() / subjects_path / str(id)
+    participant_path.mkdir(parents=True, exist_ok=True)
+    for index, i in enumerate(temp):
+        if not temp[i].empty:
+            df = temp[i].dropna().to_dict()
+            for i in df:
+                d = json.loads(df[i])
+                df = pd.DataFrame.from_dict(d, orient="index").T
+                arrX = df['arrX']
+                arrY = df['arrY']
+                arrTime = df['arrTime']
+                arrXD = df['arrXD']
+                arrYD = df['arrYD']
+                arrTimeD = df['arrTimeD']
+                arrMD = df['arrMD']
+                arrMU = df['arrMU']
+                mytext = ""
+
+                if arrX.size > 0:
+                    for i in range(0,arrX.size) :
+                        mytext = mytext + str(arrTime[i]) + "," + str(arrX[i]) + "," +  str(arrY[i]) + "\n"
+
+                mytext = mytext + "-1,-1,-1\n";
+
+                if arrY.size > 0:
+                    for i in range(0,arrY.size) :
+                        mytext = mytext + str(arrTimeD[i]) + "," + str(arrXD[i]) + "," +  str(arrYD[i]) + "\n"
+
+                mytext = mytext + "-1,-1,-1\n";
+
+                if arrMD.size > 0:
+                    for i in range(0,arrMD.size):
+                        mytext = mytext + str(arrMD[i]) + ",,\n"
+
+                mytext = mytext + "-1,-1,-1\n";
+
+                if arrMU.size > 0:
+                    for i in range(0,arrMU.size):
+                        mytext = mytext + str(arrMU[i]) + ",,\n"
+
+                mytext = mytext.replace("NaN", "")
+                mytext = mytext.replace("nan", "")
+                print(mytext, file = open(participant_path / (str(index) + '.csv'), 'w'))
+                print(presentation, file=open(participant_path / "presentation.txt", 'w'))
+                print(p, file=open(participant_path / "participant_id.txt", 'w'))
---
+++ readme.md
@@ -0,0 +1,113 @@
+<div id="top"></div>
+<!--
+*** Thanks for checking out the Best-README-Template. If you have a suggestion
+*** that would make this better, please fork the repo and create a pull request
+*** or simply open an issue with the tag "enhancement".
+*** Don't forget to give the project a star!
+*** Thanks again! Now go create something AMAZING! :D
+-->
+
+
+
+<!-- PROJECT SHIELDS -->
+<!--
+*** I'm using markdown "reference style" links for readability.
+*** Reference links are enclosed in brackets [ ] instead of parentheses ( ).
+*** See the bottom of this document for the declaration of the reference variables
+*** for contributors-url, forks-url, etc. This is an optional, concise syntax you may use.
+*** https://www.markdownguide.org/basic-syntax/#reference-style-links
+--
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+[![Product Name Screen Shot][product-screenshot]](https://example.com)
+
+This project is an oTree re-implementation of Enrico Glerean's [![embody survey software]][Enrico-url]
+
+Why?
+* Original Software uses csv files instead of a database.
+* Compataibility with Heroku.
+* oTree makes it simple to modify and extend the software.
+
+
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+### Built With
+
+This section should list any major frameworks/libraries used to bootstrap your project. Leave any add-ons/plugins for the acknowledgements section. Here are a few examples.
+
+* [![oTree][otree.readthedocs.io]][Next-url]
+* [![Bootstrap][Bootstrap.com]][Bootstrap-url]
+* [![JQuery][JQuery.com]][JQuery-url]
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+<!-- USAGE EXAMPLES -->
+## Usage
+
+Following the Survey , use the convert.py to make the exported CSV from the oTree "Data" tab useable in Enrico Glerean's [![Matlab code]][Enrico-url2]
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+<!-- ROADMAP -->
+## Roadmap
+
+- [] Streamlining
+- [] R re-implementation of Matlab Code
+
+
+
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+<!-- CONTRIBUTING -->
+## Contributing
+
+Contributions are what make the open source community such an amazing place to learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+If you have a suggestion that would make this better, please fork the repo and create a pull request. You can also simply open an issue with the tag "enhancement".
+Don't forget to give the project a star! Thanks again!
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+<!-- LICENSE -->
+## License
+
+Distributed under the MIT License. See `LICENSE.txt` for more information.
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+
+
+
+
+
+
+<!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+[JQuery.com]: https://img.shields.io/badge/jQuery-0769AD?style=for-the-badge&logo=jquery&logoColor=white
+[JQuery-url]: https://jquery.com 
+[oTree-url]: https://otree.readthedocs.io/
+[Bootstrap-url]: https://getbootstrap.com/
+[Enrico-url]: https://version.aalto.fi/gitlab/eglerean/embody
+[Enrico-url2]: https://version.aalto.fi/gitlab/eglerean/embody/-/tree/master/matlab
